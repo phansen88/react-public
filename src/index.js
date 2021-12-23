263533a23bbd71a658b20c932dec079b1383f9f9
Peter <phansen88@gmail.com>
2021-12-23 15:29:57 +0100

    new update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import './css/index.css';
+import logo from './logo.svg';
+import './css/App.css';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +21,53 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          No need
+        </p>
+        <section>
+      <Router>
+      <div>
+        <h1>hello</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Topics</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/about" element={<About/>} />
+          <Route path="/"  element={<Home/>} /> 
+        </Routes>
+      </div>
+    </Router>
+      </section>
+      </header>
+      
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+reportWebVitals(console.log);
